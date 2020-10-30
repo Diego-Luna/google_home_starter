e3df67afa999833013dc331c1aec15620c22705e
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2020-10-30 11:03:50 -0600

    activating the eyes

--- public/python/sw1_on.py
+++ public/python/sw1_on.py
@@ -1,6 +1,7 @@
 # Edit line 6 to match your chosen GPIO pin
-
 import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT)
+
+GPIO.output(16, True)
